4cc20b9cbfd0b32f64ce63a921efcaa666d92475
Alan Dias <adias7882@gmail.com>
2020-09-13 21:47:43 -0300

    Projeto Team Meeting feito (com atraso! hehe). Pra CIIMA

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,3 +1,11 @@
-const fibonacci = () => {};
+const fibonacci = (a) => {
+    if (a === 0) { 
+        return 1
+    } else if (a === 1) {
+        return 1
+    } else {
+        return fibonacci(a-1) + fibonacci(a-2)
+    }
+};
 
 module.exports = fibonacci;
